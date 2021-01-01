7ab201e63d6e7965de901bbc223e60b160079c0c
zoloft <zoloft@orcod.io>
2021-01-01 14:47:44 +0100

    Added display.py directly instead of symlinking it

--- nanpy/display.py
+++ nanpy/display.py
@@ -1,1 +1,42 @@
-../../nanpy-ext/display.py
+from nanpy.arduinoboard import ArduinoObject
+from nanpy.arduinoboard import (arduinoobjectmethod, returns)
+
+class Display(ArduinoObject):
+    def __init__(self, connection=None):
+        ArduinoObject.__init__(self, connection=connection)
+        self.id = self.call('new')
+
+    @arduinoobjectmethod
+    def setSeconds(self, seconds):
+        pass
+
+    @returns(int)
+    @arduinoobjectmethod
+    def getSeconds(self):
+        pass
+
+    @arduinoobjectmethod
+    def setMinutes(self, minutes):
+        pass
+
+    @returns(int)
+    @arduinoobjectmethod
+    def getMinutes(self):
+        pass
+
+    @arduinoobjectmethod
+    def setHours(self, hours):
+        pass
+
+    @returns(int)
+    @arduinoobjectmethod
+    def getHours(self):
+        pass
+
+    @arduinoobjectmethod
+    def setup(self):
+        pass
+
+    @arduinoobjectmethod
+    def loop(self):
+        pass
